bff27526ff351a938e9a24d208ec403fcf75424d
elietemirandazup <83075129+elietemirandazup@users.noreply.github.com>
2023-05-17 22:29:11 -0300

    Update login.cy.js usando o stub para alterar o status code com o comando intercept

--- cypress/e2e/3-alura-pic/login.cy.js
+++ cypress/e2e/3-alura-pic/login.cy.js
@@ -2,10 +2,14 @@ describe('Login e registros de usuários Alura pic', () => {
 
     beforeEach(() => {
         cy.visit('https://alura-fotos.herokuapp.com/')
+        cy.intercept('POST', 'https://apialurapic.herokuapp.com/user/login', {
+            statusCode: 400
+        }).as('stubPost')
     })
 
     it('fazer login de usuario valido', () => {
         cy.login(Cypress.env('userName'), Cypress.env('password'))
+        cy.wait('@stubPost')
         cy.contains('a', '(Logout)').should('be.visible');
     })
 
